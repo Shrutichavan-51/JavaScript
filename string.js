f933fdaa9320c1a277c8e9780589a61e089858b7
Shruti Chavan <rshrutichavan@gmail.com>
2025-03-02 20:55:41 +0530

    Delete string.js

--- string.js
+++
@@ -1,8 +0,0 @@
-const name = "darshan"
-const repoFt = 5
-
-// console.log(name + repoFt + " value");    old tyoe to represent
-
-console.log(`hello my name is s ${name} and my repo coumt is ${repoFt}`);   // string interpolstion  (``)
-
-
